6fe805d1213d26c508331bcc551418cd4ff795c9
Dreeam <61569423+Dreeam-qwq@users.noreply.github.com>
2025-11-30 01:27:16 -0500

    fix: 修复无法正确获取 `IS_CHINA_SITE` 环境变量 (#20)
    修复浏览 `/services` 页面时无法获取 NodeJS的 process 从而无法正确获取 IS_CHINA_SITE 环境变量，参考 https://docusaurus.io/docs/deployment#using-environment-variables

--- src/components/GlobalContent.js
+++ src/components/GlobalContent.js
@@ -1,15 +1,13 @@
 import React from "react";
 import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
 
-const IS_CHINA_SITE = process.env.CHINA === "true";
-
 /**
- * A component that conditionally renders its children based on the CHINA environment variable.
- * If CHINA is 'true', the children (typically Markdown content) will not be rendered.
+ * A component that conditionally renders its children based on the `IS_CHINA_SITE` environment variable.
+ * If `IS_CHINA_SITE` is 'true', the children (typically Markdown content) will not be rendered.
  */
 export default function GlobalContent({ children }) {
-    if (IS_CHINA_SITE) {
-        return null; // Do not render children if in China site context
+    if (useDocusaurusContext().siteConfig.customFields.IS_CHINA_SITE) {
+        return null; // Do not render children if in the China site context
     }
     return <>{children}</>; // Render children otherwise
 }
